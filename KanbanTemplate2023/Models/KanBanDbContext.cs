3b29705714909c202bcd4acc9e5ce15a2c4533ed
dimitris <iracleous@gmail.com>
2023-02-16 10:11:13 +0200

    Add project files.

---
+++ KanbanTemplate2023/Models/KanBanDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KanbanTemplate2023.Models
+{
+    public class KanBanDbContext:DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+    
+
+    public KanBanDbContext(
+        DbContextOptions<KanBanDbContext> 
+        options) : base(options)
+    { }
+
+}
+
+}
---
+++ KanbanTemplate2023/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace KanbanTemplate2023.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string ?Name { get; set; }
+
+    }
+}
